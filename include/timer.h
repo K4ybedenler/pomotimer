0052d0f86cb373bf980142c4fa107350439aea88
K4ybedenler <kaybedenlerpaul@gmail.com>
2024-12-20 19:57:21 -0300

    added db logic

--- include/timer.h
+++ include/timer.h
@@ -2,6 +2,8 @@
 #include <QPainter>
 #include <QPen>
 #include <QLabel>
+#include <chrono>
+#include <sqlite3.h>
 
 #ifndef TIMER_H
 #define TIMER_H
@@ -13,6 +15,7 @@ class Timer : public QWidget
 public:
     explicit Timer(QWidget *parent = nullptr);
     ~Timer();
+
     void zeroProgress();
     void pauseTimer();
     void startTimer();
@@ -27,6 +30,56 @@ private:
     double progress;
     int totalSeconds;
     int elapsedSeconds;
+
+    std::chrono::steady_clock::time_point startTime, roundFinishTime;
+    std::chrono::system_clock::time_point startTimeDB, roundFinishTimeDB, finishTime;
+
+    void printDuration(long long int duration);
+    void updateTimer();
+    void finishRound();
+    void dbPrepare(const char *table_name);
+    void queryPrepare(const char *query_tmpl, sqlite3_stmt *&stmt);
+    sqlite3 *db;
+    sqlite3_int64 lastId;
+    sqlite3_stmt *stmtLaunches, *stmtRounds, *stmtLastId;
+    int roundNumber = 1;
+
+    void bindStatement(sqlite3_stmt *stmt,
+                        int columnNumber,
+                        std::chrono::system_clock::time_point timePoint);
+
+    void bindStatement(sqlite3_stmt *stmt,
+                        int columnNumber,
+                        sqlite3_int64 value);
+
+    void pushStatement(sqlite3_stmt *stmt);
+
+    const char *insertLaunch = "INSERT INTO launches ("
+                                "launch_id, "
+                                "start_time, "
+                                "finish_time) "
+                                "VALUES (?, ?, ?)";
+
+    const char *insertRound = "INSERT INTO rounds ("
+                               "round_id, "
+                               "launch_id, "
+                               "round_number, "
+                               "round_time) "
+                               "VALUES (?, ?, ?, ?)";
+
+    const char *lastLaunchId = "SELECT MAX(launch_id) FROM launches;";
+
+    const char *sqlLaunches = "CREATE TABLE IF NOT EXISTS launches ("
+                               "launch_id INTEGER PRIMARY KEY, "
+                               "start_time INTEGER, "
+                               "finish_time INTEGER);";
+
+    const char *sqlRounds = "CREATE TABLE IF NOT EXISTS rounds ("
+                             "round_id INTEGER PRIMARY KEY, "
+                             "launch_id INTEGER, "
+                             "round_number INTEGER, "
+                             "round_time INTEGER);";
+
 };
 
 #endif
--- src/timer.cpp
+++ src/timer.cpp
@@ -1,12 +1,31 @@
 #include <timer.h>
 #include <QTimer>
+#include <sqlite3.h>
 
 Timer::Timer(QWidget *parent)
     : QWidget(parent), totalSeconds(1800), elapsedSeconds(0) {
     timer = new QTimer(this);
+
+    if (sqlite3_open("pomobase.db", &db)) {
+        qDebug() << "can't open the db: " << sqlite3_errmsg(db);
+    }
+
+    dbPrepare(sqlLaunches);
+    dbPrepare(sqlRounds);
+    queryPrepare(insertLaunch, stmtLaunches);
+    queryPrepare(insertRound, stmtRounds);
+    queryPrepare(lastLaunchId, stmtLastId);
 }
 
-Timer::~Timer(){}
+Timer::~Timer(){
+    if(elapsedSeconds) {
+//        stop_pomo();
+    }
+    sqlite3_finalize(stmtLaunches);
+    sqlite3_finalize(stmtRounds);
+    sqlite3_finalize(stmtLastId);
+    sqlite3_close(db);
+}
 
 void Timer::startCount(){
     elapsedSeconds++;
@@ -25,10 +44,34 @@ int Timer::getTotalSeconds(){
 
 void Timer::stopTimer(){
     elapsedSeconds = 0;
+
+    auto currentTime = std::chrono::steady_clock::now();
+    finishTime = std::chrono::system_clock::now();
+    auto timeSpan = std::chrono::duration_cast<std::chrono::milliseconds>(
+                         currentTime - startTime
+                         ).count();
+    auto timeSpanRnd = std::chrono::duration_cast<std::chrono::milliseconds>(
+                             currentTime - roundFinishTime
+                             ).count();
+    roundFinishTime = std::chrono::steady_clock::now();
+    bindStatement(stmtLaunches, 3, finishTime);
+    pushStatement(stmtLaunches);
+    if(sqlite3_step(stmtLastId) == SQLITE_ROW) {
+        lastId = sqlite3_column_int64(stmtLastId, 0);
+    }
+    bindStatement(stmtRounds, 2, lastId);
+    bindStatement(stmtRounds, 3, roundNumber);
+    bindStatement(stmtRounds, 4, timeSpanRnd);
+    pushStatement(stmtRounds);
+    sqlite3_close(db);
 }
 
 void Timer::startTimer(){
     timer->start(1000);
+    roundFinishTime = startTime = std::chrono::steady_clock::now();
+    roundFinishTimeDB = startTimeDB = std::chrono::system_clock::now();
+    bindStatement(stmtLaunches, 2, startTimeDB);
+    lastId = sqlite3_last_insert_rowid(db);
 }
 
 void Timer::pauseTimer(){
@@ -39,3 +82,57 @@ QTimer *Timer::getTimer(){
     return timer;
 }
 
+void Timer::dbPrepare(const char *tableName)
+{
+    char *errMsg = 0;
+    if (sqlite3_exec(db, tableName, 0, 0, &errMsg) != SQLITE_OK) {
+        qDebug() << "SQL error: " << errMsg;
+        sqlite3_free(errMsg);
+        sqlite3_close(db);
+    }
+}
+
+void Timer::queryPrepare(const char *queryTmpl, sqlite3_stmt *&stmt)
+{
+    if (sqlite3_prepare_v2(db, queryTmpl, -1, &stmt, 0) != SQLITE_OK) {
+        qDebug() << "failed to prepare SQL statement(command): " << sqlite3_errmsg(db);
+    }
+}
+
+void Timer::bindStatement(sqlite3_stmt *stmt,
+                                int column_number,
+                                std::chrono::system_clock::time_point time_point)
+{
+    sqlite3_int64 sqliteTimePoint = std::chrono::duration_cast<std::chrono::milliseconds>(
+                                        time_point.time_since_epoch()).count();
+    sqlite3_bind_int64(stmt, column_number, sqliteTimePoint);
+}
+
+void Timer::bindStatement(sqlite3_stmt *stmt,
+                                int columnNumber,
+                                sqlite3_int64 value)
+{
+    sqlite3_bind_int64(stmt, columnNumber, value);
+}
+
+void Timer::finishRound() {
+    auto current_time = std::chrono::steady_clock::now();
+    auto timeSpan = std::chrono::duration_cast<std::chrono::milliseconds>(
+                         current_time - roundFinishTime
+                         ).count();
+    roundFinishTime = std::chrono::steady_clock::now();
+    if(sqlite3_step(stmtLastId) == SQLITE_ROW) {
+        lastId = sqlite3_column_int64(stmtLastId, 0) + 1;
+    }
+    bindStatement(stmtRounds, 2, lastId);
+    bindStatement(stmtRounds, 3, roundNumber);
+    bindStatement(stmtRounds, 4, timeSpan);
+    pushStatement(stmtRounds);
+    roundNumber++;
+}
+
+void Timer::pushStatement(sqlite3_stmt *stmt) {
+    sqlite3_step(stmt);
+    sqlite3_reset(stmt);
+    sqlite3_clear_bindings(stmt);
+}
